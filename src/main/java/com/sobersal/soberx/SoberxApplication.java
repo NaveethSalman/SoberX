ea30da6652ea13061b861c900990cf00dd8c4454
NaveethSalman <naveethsalman25@gmail.com>
2025-04-02 14:25:39 +0530

    First config(Initialization)

---
+++ src/main/java/com/sobersal/soberx/SoberxApplication.java
@@ -0,0 +1,13 @@
+package com.sobersal.soberx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoberxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoberxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sobersal/soberx/SoberxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sobersal.soberx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoberxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
